945b23b5ca082e79c9cee9405ccb30dded6462fe
Dearborn42 <amurf26@outlook.com>
2023-08-18 15:09:41 -0700

    working on student creation

--- mongo.js
+++ mongo.js
@@ -1,2 +1,20 @@
-// set up mongo database
-// only need to export functions and don't need to create a fetch function for the database
+import dotenv from 'dotenv';
+import { Collection, MongoClient } from 'mongodb';
+
+dotenv.config();
+
+const client = await MongoClient.connect(process.env.MONGO);
+const collection = client.db('ClusterDB').collection('students');
+
+export class Student{
+    constructor(name, grade_level, school){
+        Object.assign(this, {});
+    }
+}
+
+export async function createStudent(){
+    let student = new Student("Andrew Murphy", 12)
+    await collection.insertOne(student)
+}
+
+createStudent();
