f91c7d33b3307f2d3b042d581c1c9c22a1532147
Dearborn42 <amurf26@outlook.com>
2023-08-21 15:16:43 -0700

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -7,15 +7,30 @@ const client = await MongoClient.connect(process.env.MONGO);
 const collection = client.db('ClusterDB').collection('students');
 
 export class Student{
-    constructor(name, grade_level, school){
+    constructor(name, grade_level, school, fgrades, sophgrades, jgrades, sengrades){
+        let grades = this.setGrades(fgrades, sophgrades, jgrades, sengrades)
         Object.assign(this, {
-            
+            name, 
+            grade_level, 
+            school, 
+            "freshman-grades": grades[0], 
+            "sophmore-grades": grades[1],
+            "junior-grades": grades[2],
+            "senior-grades": grades[3],
         });
+    }
+    setGrades(f, so, j, se){
+        let grades = [];
+        let allgrades = [f, so, j, se]
+        allgrades.map(x => {
+            grades.push({"math": x[0]||"N/A", "science": x[1]||"N/A", "english": x[2]||"N/A", "history": x[3]||"N/A"})
+        })
+        return this && grades
     }
 }
 
 export async function createStudent(){
-    let student = new Student("Andrew Murphy", 12)
+    let student = new Student("Andrew Murphy", 12, "Thunderbird", [23, 45, 67, 89], [12, 34, 56, 78], [9, 21, 43, 65], [87, 65, 43, 21])
     await collection.insertOne(student)
 }
 
