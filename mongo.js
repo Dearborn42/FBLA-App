e8c059edbdfebd33cac1481280907eae0bde727d
Dearborn42 <amurf26@outlook.com>
2023-08-23 15:20:09 -0700

    Expanded examples for easier reading

--- mongo.js
+++ mongo.js
@@ -2,17 +2,32 @@ import {createStudent} from './Student_Scripts/studentCreation.js';
 import {updateName, updateGradeLvl, updateSchool, updateLetterGrades} from './Student_Scripts/updateStudent.js'
 
 
-// await createStudent("Andrew Murphy", 12, "Thunderbird");
+/*await createStudent(Fill in at least Full Name, grade, and school)
+await createStudent("Andrew Murphy", 12, "Thunderbird");
 
+await updateName(
+    String:"Full name of student", 
+    String:"Full new name of student"
+);
+await updateName("Andrew Murphy", "Jonathan lam");
 
+await updateGradeLvl(
+    String:"Full name of student", 
+    Number: new grade level
+);
+await updateGradeLvl("Jonathan lam", 10);
 
-// await updateName(String:"Full name of student", String:"Full new name of student");
-// await updateName("Andrew Murphy", "Jonathan lam");
+await updateSchool(
+    String:"Full name of student", 
+    String:"Full new name of School"
+);
+await updateSchool("Jonathan lam", "O'Connor");
 
-
-// await updateGradeLvl("Jonathan lam", 10);
-
-// await updateSchool("Jonathan lam", "O'Connor");
-
-// await updateLetterGrades("Jonathan lam", "freshman", "math", 98);
-
+await updateLetterGrades(
+    String:"Full name of student", 
+    String:"name of grade level", 
+    String:" Name of class", 
+    Number: new grade
+):
+await updateLetterGrades("Jonathan lam", "freshman", "math", 98);
+*/
