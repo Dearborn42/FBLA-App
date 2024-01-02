a8ede72d08448feb8382d34ebc12e33c4fe799c1
Dearborn42 <amurf26@outlook.com>
2024-01-02 15:28:16 -0700

    Update Clubs component finished
    Can now update clubs just by Typing in inputs and can now add or remove clubs by just hitting repective buttons

--- Acadar/Components/Update/UpdateClubs.js
+++ Acadar/Components/Update/UpdateClubs.js
@@ -3,6 +3,7 @@ import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 
 export default function UpdateClubs({user}){
+    const [newForm, setNewForm] = useState(false);
     const [form, setForm] = useState({clubs: user.clubs});
     const [newClub, setNewClub] = useState({"name": '', "desc": ''});
     const [field, setField] = useState("");
@@ -10,7 +11,11 @@ export default function UpdateClubs({user}){
 
     var handleValue = (text) => setValue(text);
     var handleField = (textValue) => setField(textValue);
-    var handleNewClub = (field, value) => setNewClub({[field]: value});
+    var handleNewClub = (field, value) => setNewClub((prev) => {return {...prev, ...{[field]: value}}});
+    var handleForm = () => {
+        setNewClub({"name": '', "desc": ''})
+        setNewForm((prev) => !prev);
+    };
 
     const addClub = async() => {
         var body1 = await fetch(`http://localhost:5000/clubs/add`, {
@@ -37,11 +42,12 @@ export default function UpdateClubs({user}){
         }
     };
     const updateClub = async (name) => {
+        if(value === "") return;
         var body1 = await fetch(`http://localhost:5000/clubs/update/${name}/${field}`, {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             credentials: "include",
-            body: JSON.stringify(newClub)
+            body: JSON.stringify({"value": value})
         });
         var body1_response = await body1.json();
         if (body1_response.success){
@@ -57,7 +63,8 @@ export default function UpdateClubs({user}){
               <Text>Club {index + 1}</Text>
               <TextInput
                 style={styles.input}
-                onChangeText={(text) => handleValue(text)}
+                onChangeText={(text) => {handleValue(text); handleField("name")}}
+                onBlur={() => updateClub(club.name)}
                 placeholder={club.name}
                 required
                 id={"name"}
@@ -65,7 +72,8 @@ export default function UpdateClubs({user}){
               />
               <TextInput
                 style={styles.input}
-                onChangeText={(text) => handleValue(text)}
+                onChangeText={(text) => {handleValue(text); handleField("desc")}}
+                onBlur={() => updateClub(club.name)}
                 placeholder={club.desc}
                 required
                 id={"desc"}
@@ -73,8 +81,36 @@ export default function UpdateClubs({user}){
               />
               <Button title="Remove" onPress={() => removeClub(club.name)} />
             </View>
-        ))}
-            <Button title={`Add Club`} onPress={() => addClub()} />
+        ))} 
+            {newForm && (
+                <View>
+                <TextInput 
+                    style={styles.input} 
+                    type="text" 
+                    placeholder="Club Name"
+                    onChangeText={(text) => handleNewClub("name", text)}
+                    id={"name"}
+                    name={"name"} 
+                />
+                <TextInput 
+                    style={styles.input} 
+                    type="text" 
+                    placeholder="Club Description"
+                    onChangeText={(text) => handleNewClub("desc", text)}
+                    id={"desc"}
+                    name={"desc"}  
+                />
+                <Button 
+                    title="Submit"
+                    onPress={addClub}
+                />
+                </View>
+            )}
+            {!newForm ? (
+                <Button title={`Add Club`} onPress={handleForm} />
+            ) : (
+                <Button title={`Cancel Club`} onPress={handleForm} />
+            )}
         </View>
     </View> 
   )
